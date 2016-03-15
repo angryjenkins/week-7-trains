e67586ebe6f3d7668174369dce7b02c13c4de4cf
@angryjenkins <matt@angryjenkins.com>
2016-03-15 01:25:13 -0400

    mon night push - firebase error, negative remainder?

--- scripts.js
+++ scripts.js
@@ -1,5 +1,52 @@
-var trainTracker = new Firebase("https://mattmartintrains.firebaseio.com/");
+var trainTracker = new Firebase("https://mattmartintrains.firebaseio.com");
 
+function pushData(){
+	var name = $("#name").val().trim();
+	var dest = $("#destination").val().trim();
+	var firstTime = $("#first").val().trim();
+	var freq = $("#frequency").val().trim();
+
+	var firstTimeConverted = moment(firstTime, "HH:mm");
+		console.log(moment.unix(firstTimeConverted));
+
+	console.log(name);
+	console.log(dest);
+	console.log(firstTime);
+	console.log(freq);
+
+	var diffTime = moment().diff(moment.unix(firstTimeConverted), "minutes");
+	console.log(diffTime);
+
+	var tRemainder = diffTime % freq;
+	console.log(tRemainder);
+
+	var minutesTill = freq - tRemainder;
+	console.log(minutesTill);
+
+	var nextTrain = moment().add(minutesTill, "minutes")
+	console.log(moment(nextTrain).format("HH:mm"));
+
+
+	trainTracker.push({
+	  name: name,
+	  destination: dest,
+	  first: firstTime,
+	  frequency: freq,
+	  next: nextTrain,
+	  minutesTo: minutesTill
+	})
+
+	$("#name").val("");
+	$("#destination").val("");
+	$("#first").val("");
+	$("#frequency").val("");
+};
+
+trainTracker.on("child_added", function(snap){
+    $("#results").append("<tr><td>" + snap.val().name + "</td><td>" + snap.val().destination + "</td><td>" + snap.val().frequency + "</td><td>" + snap.val().next + "</td><td>" + snap.val().minutesTo + "</td></tr>");
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
 $("#go").click(function(){
   pushData();
@@ -8,47 +55,4 @@ $(window).keyup(function(e) {
   if(e.keyCode == 13){
     pushData();
   }
-});
-
-function pushData(){
-  var name = $("#name").val().trim();
-  var dest = $("#destination").val().trim();
-  var firstArrival = new Date($("#first").val().trim());
-  var freq = $("#frequency").val().trim();
-  
-  if(name != "" && dest != "" && firstArrival != "" && freq != ""){
-    var diffTime = moment().diff(moment.unix(firstArrival), "minutes");
-	console.log(diffTime);
-
-	var tRemainder = diffTime % freq;
-	console.log(tRemainder);
-    
-    var minutesTill = freq - tRemainder;
-	console.log(minutesTill);
-
-	var nextTrain = moment().add(minutesTill, "minutes")
-	console.log(moment(nextTrain).format("HH:mm"));
-
-    $("#name").val("");
-    $("#destination").val("");
-    $("#first").val("");
-    $("#frequency").val("");
-
-  
-
-    trainTracker.push({
-      name: name,
-      destination: dest,
-      first: firstArrival,
-      frequency: freq,
-      minutesTo: minutesTill,
-      next: nextTrain
-    });
-  }
-}
-
-trainTracker.on("child_added", function(snap){
-    $("#results").append("<tr><td>"+snap.val().name + "</td><td>"+snap.val().destination +"</td><td>" + snap.val().frequency + "</td><td>" + snap.val().next + "</td><td>" + snap.val().minutesTo + "</td></tr>");
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
 });
