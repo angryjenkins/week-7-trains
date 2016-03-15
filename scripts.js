b9cd6b3b5550648886c4d0ee4d0cff04ec051cfd
Matthew N. Martin <matt@angryjenkins.com>
2016-03-15 17:48:21 -0400

    no firebase errors - need to calc minutes and next train locally

--- scripts.js
+++ scripts.js
@@ -1,16 +1,28 @@
 var trainTracker = new Firebase("https://mattmartintrains.firebaseIO.com");
 
-function pushData(){
-	
-	console.log(moment.unix(firstTimeConverted));
+var name, dest, firstTime, frew, nextTrain, minutesTill;
 
+function pushData(){
+
+	var name = $("#name").val().trim();
+	var dest = $("#destination").val().trim();
+	var firstTime = $("#first").val().trim();
+	var freq = $("#frequency").val().trim();
+	
 	console.log(name);
 	console.log(dest);
 	console.log(firstTime);
 	console.log(freq);
+	console.log('---------');
+	
+	var firstTimeConverted = moment(firstTime,"hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+	
+	console.log(moment.unix(firstTimeConverted));
 
-	var diffTime = moment().diff(moment.unix(firstTimeConverted), "minutes");
-	console.log(diffTime);
+
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
 
 	var rem = diffTime % freq;
 	console.log(rem);
@@ -20,6 +32,8 @@ function pushData(){
 
 	var nextTrain = moment().add(minutesTill, "minutes");
 	console.log(moment(nextTrain).format("HH:mm"));
+
+	localStorage
 
 
 	trainTracker.push({
